7506ad3827cdda83bdfd90a83004f68e886d392f
zleonardo0 <164013040+zleonardo0@users.noreply.github.com>
2024-10-10 12:05:33 -0300

    Update perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript',
+    'O JavaScript é uma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'Programação',
+    'o que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês se diz HI(RAI)'
+)
